5851b8371a5d0d5103d6c6081d333e6277f6c566
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-02-03 12:35:11 +0900

    add title screen

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ function Obstacle(width, height, position) {
 
 const FPS = 60;
 const FC = new FrameCounter(FPS);
+const GAME_TITLE = 0;
 const GAME_PLAYING = 1;
 const GAME_OVER = -1;
 
@@ -63,7 +64,7 @@ const INIT_VELOCITY = 12;
 const MIN_INTERVAL = 20;
 const MAX_INTERVAL = 55;
 
-let gameState = GAME_PLAYING;
+let gameState = GAME_TITLE;
 let gameOverFrame = undefined;
 
 const GROUND_MARGIN = 200;
@@ -76,15 +77,17 @@ const player = new Player();
 const obs = [];
 const clouds = [];
 
-let tailX = 0;
+let tailX = 25;
 let ctail = 0;
 let prev = 0;
 let prevFrame = -1;
 let collided = false;
 
 const dino_img = new Image();
+const title_img = new Image();
 const gameover_img = new Image();
 dino_img.src = player.run_img_src[0];
+title_img.src = 'img/gamestart.png';
 gameover_img.src = 'img/gameover.png';
 
 
@@ -98,37 +101,7 @@ function tick(t = new Date().getTime()) {
 	// canvas clear
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	// draw sky
-	ctx.fillStyle = '#a0d8ef';
-	ctx.fillRect(0, 0, canvas.width, GROUND_MARGIN);
-
-	// draw ground
-	ctx.fillStyle = "#79c06e";
-	ctx.fillRect(0, GROUND_MARGIN, canvas.width, canvas.height);
-	ctx.beginPath();
-	ctx.moveTo(0, GROUND_MARGIN);
-	ctx.lineTo(canvas.width, GROUND_MARGIN);
-	ctx.stroke();
-
-	// draw obstacle
-	for (let i = 0; i < obs.length; i++) {
-		let screenX = (STAGE_RATIO * (obs[i].position.x - distance)) + CHARACTER_MARGIN;
-		let screenY = GROUND_MARGIN;
-		ctx.rect(screenX, screenY + PERSPECTIVE_MARGIN, obs[i].width, -obs[i].height);
-
-		if (screenX < -(obs[i].width + 10)) obs.splice(i, 1);
-	}
-
-	// draw clouds
-	for (let i = 0; i < clouds.length; i++) {
-		let screenX = (STAGE_RATIO * (clouds[i].x - distance) * 0.35);
-		let screenY = clouds[i].y;
-		const cloud_img = new Image();
-		cloud_img.src = 'img/cloud.png';
-		ctx.drawImage(cloud_img, screenX, screenY, 50, 50);
-
-		if (screenX < -50) clouds.splice(i, 1);
-	}
+	draw();
 
 	// generate obstacles
 	if (obs.length < 50) {
@@ -142,25 +115,6 @@ function tick(t = new Date().getTime()) {
 		let y = getRandomInt(0, 50);
 		clouds.push(new Vector2(ctail, 30 + y))
 	}
-
-	// draw objects
-	ctx.fillStyle = (collided) ? "red" : "black";
-	ctx.fill();
-
-	// draw character
-	ctx.drawImage(dino_img, CHARACTER_MARGIN, GROUND_MARGIN + player.position.y - CHARACTER_HEIGHT + PERSPECTIVE_MARGIN + 5, CHARACTER_WIDTH, CHARACTER_HEIGHT);
-
-	// draw score
-	ctx.font = "bold 22px monospace";
-	ctx.strokeStyle = "black";
-	ctx.fillStyle = "white";
-	ctx.textAlign = "right";
-	ctx.lineWidth = 1.5;
-	ctx.fillText(`SCORE: ${player.score}`, canvas.width - 100, 30);
-	ctx.strokeText(`SCORE: ${player.score}`, canvas.width - 100, 30);
-	ctx.textAlign = "left";
-	ctx.fillText(`HI SCORE: ${MAX_SCORE}`, 50, 30);
-	ctx.strokeText(`HI SCORE: ${MAX_SCORE}`, 50, 30);
 
 	calculate(t);
 	requestAnimationFrame(tick);
@@ -238,6 +192,59 @@ function calculate(t) {
 	}
 }
 
+function draw() {
+	// draw sky
+	ctx.fillStyle = '#a0d8ef';
+	ctx.fillRect(0, 0, canvas.width, GROUND_MARGIN);
+
+	// draw ground
+	ctx.fillStyle = "#79c06e";
+	ctx.fillRect(0, GROUND_MARGIN, canvas.width, canvas.height);
+	ctx.beginPath();
+	ctx.moveTo(0, GROUND_MARGIN);
+	ctx.lineTo(canvas.width, GROUND_MARGIN);
+	ctx.stroke();
+
+	// draw obstacle
+	for (let i = 0; i < obs.length; i++) {
+		let screenX = (STAGE_RATIO * (obs[i].position.x - distance)) + CHARACTER_MARGIN;
+		let screenY = GROUND_MARGIN;
+		ctx.rect(screenX, screenY + PERSPECTIVE_MARGIN, obs[i].width, -obs[i].height);
+
+		if (screenX < -(obs[i].width + 10)) obs.splice(i, 1);
+	}
+
+	// draw clouds
+	for (let i = 0; i < clouds.length; i++) {
+		let screenX = (STAGE_RATIO * (clouds[i].x - distance) * 0.35);
+		let screenY = clouds[i].y;
+		const cloud_img = new Image();
+		cloud_img.src = 'img/cloud.png';
+		ctx.drawImage(cloud_img, screenX, screenY, 50, 50);
+
+		if (screenX < -50) clouds.splice(i, 1);
+	}
+
+	// draw objects
+	ctx.fillStyle = (collided) ? "red" : "black";
+	ctx.fill();
+
+	// draw character
+	ctx.drawImage(dino_img, CHARACTER_MARGIN, GROUND_MARGIN + player.position.y - CHARACTER_HEIGHT + PERSPECTIVE_MARGIN + 5, CHARACTER_WIDTH, CHARACTER_HEIGHT);
+
+	// draw score
+	ctx.font = "bold 22px monospace";
+	ctx.strokeStyle = "black";
+	ctx.fillStyle = "white";
+	ctx.textAlign = "right";
+	ctx.lineWidth = 1.5;
+	ctx.fillText(`SCORE: ${player.score}`, canvas.width - 100, 30);
+	ctx.strokeText(`SCORE: ${player.score}`, canvas.width - 100, 30);
+	ctx.textAlign = "left";
+	ctx.fillText(`HI SCORE: ${MAX_SCORE}`, 50, 30);
+	ctx.strokeText(`HI SCORE: ${MAX_SCORE}`, 50, 30);
+}
+
 // add control listener
 slider.addEventListener("input", () => {
 	STAGE_WIDTH = slider.value;
@@ -254,10 +261,13 @@ window.addEventListener("load", () => {
 });
 
 window.addEventListener("keydown", jump);
+window.addEventListener("keydown", gameStart);
 canvas.addEventListener("mousedown", jump);
+canvas.addEventListener("click", gameStart);
 canvas.addEventListener("click", retry);
 
 function jump(e) {
+	if (gameState != GAME_PLAYING) return;
 	if (e.type == "mousedown" || e.code == "Space") {
 		if (!player.onGround) return;
 
@@ -277,9 +287,20 @@ function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
 
+function gameStart(e) {
+	if (gameState != GAME_TITLE) return;
+	if (e.type == "click" || e.code == "Space") {
+		gameState = GAME_PLAYING;
+		tick();
+	}
+}
+
 function retry(e) {
 	if (gameState != GAME_OVER) return;
 	if (e.type == "click" || e.code == "Space") location.reload();
 }
 
-tick();
+dino_img.addEventListener("load", () => {
+	draw();
+	if (gameState == GAME_TITLE) ctx.drawImage(title_img, 256, -10, 512, 256);
+});
